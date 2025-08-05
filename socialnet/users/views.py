78d177d446346f4663158c1df8fbc9f5cf7d7667
unknown <zhur_zhur@ukr.net>
2025-08-05 18:06:46 +0300

    create respone login

--- socialnet/users/views.py
+++ socialnet/users/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse
 from django.shortcuts import render
 
 from users.forms import LoginForm
@@ -6,5 +8,16 @@ from users.forms import LoginForm
 # Create your views here.
 
 def user_login(request):
-    form = LoginForm()
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            user =authenticate(request, username=data['username'], password=data['password'])
+            if user is not None:
+                login(request, user)
+                return HttpResponse('user logged in')
+            else:
+                return HttpResponse('user not logged in')
+    else:
+        form = LoginForm()
     return render(request, 'users/login.html', {'form': form})
