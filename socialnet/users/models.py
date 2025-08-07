169737e65a94ea10eda1a4847c004d5d5d11ea85
unknown <zhur_zhur@ukr.net>
2025-08-07 18:36:34 +0300

    user

--- socialnet/users/models.py
+++ socialnet/users/models.py
@@ -1,3 +1,14 @@
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    photo = models.ImageField(upload_to='users/%Y/%m/%d/', blank=True)
+    bio = models.TextField(max_length=500, blank=True)
+    location = models.CharField(max_length=30, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return self.user.username
--- socialnet/users/urls.py
+++ socialnet/users/urls.py
@@ -12,5 +12,7 @@ urlpatterns = [
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name='password_reset_done'),
     path('password_reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), name='password_reset_confirm'),
     path('password_reset/complete/', auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'), name='password_reset_complete'),
+    path('register/', views.register, name='register'),
+    path('edit/', views.edit_profile, name='edit'),
 
 ]
